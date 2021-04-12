c53b15a5533820efc5b5b877735e7d6fae9b4292
asegar01 <asegar01@ucm.es>
2021-04-12 19:34:33 +0200

    Creada la funcionalidad para rotar piezas

---
+++ Scrolling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scrolling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rotación");
+        }
+    }
+}
